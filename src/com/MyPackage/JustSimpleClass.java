bb6831a7d45ed013de0a9d42932d6f7a4e1e6682
tom <sobeczki@gmail.com>
2021-12-09 16:58:42 +0000

    next marged commit

--- src/com/MyPackage/JustSimpleClass.java
+++ src/com/MyPackage/JustSimpleClass.java
@@ -1,6 +1,44 @@
 package com.MyPackage;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
 public class JustSimpleClass {
 
     private String myClass;
+    private LocalDate date;
+
+    public JustSimpleClass(String myClass, LocalDate date) {
+        this.myClass = myClass;
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        JustSimpleClass that = (JustSimpleClass) o;
+        return myClass.equals(that.myClass) && date.equals(that.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(myClass, date);
+    }
+
+    public String getMyClass() {
+        return myClass;
+    }
+
+    public void setMyClass(String myClass) {
+        this.myClass = myClass;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 }
